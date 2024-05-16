45a987591337659dc7ebb812d5cf54c9b91f952b
Heekwon <seoheekwon@gmail.com>
2024-05-16 02:25:13 +0100

    Initial commit

---
+++ src/main/java/com/dugimto/dugimto/DugimtoApplication.java
@@ -0,0 +1,13 @@
+package com.dugimto.dugimto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DugimtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DugimtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dugimto/dugimto/DugimtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dugimto.dugimto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugimtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
