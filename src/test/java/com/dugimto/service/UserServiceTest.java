05be6a2e4e982662bb234b80eff8e2e7004151f7
Heekwon <seoheekwon@gmail.com>
2024-05-19 01:24:50 +0100

    test: add test for UserService

---
+++ src/test/java/com/dugimto/service/UserServiceTest.java
@@ -0,0 +1,88 @@
+package com.dugimto.service;
+
+import com.dugimto.domain.User;
+import com.dugimto.exception.UserNotFoundException;
+import com.dugimto.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User("testuser", "testuser@example.com", "password");
+    }
+
+    @Test
+    void register() {
+        //arrange
+        Long userId = userService.register(user);
+
+        //act
+        User foundUser = userRepository.findById(userId).orElse(null);
+
+        //assert
+        assertThat(foundUser).isNotNull();
+        assertThat(foundUser).isEqualTo(user);
+        assertThat(userId).isEqualTo(user.getId());
+    }
+
+    @Test
+    void findUsers() {
+        //arrange
+        userRepository.save(user);
+
+        //act
+        List<User> users = userService.findUsers();
+
+        //assert
+        assertThat(users.size()).isEqualTo(1);
+        assertThat(users).containsExactly(user);
+    }
+
+    @Test
+    public void findOne() {
+        //arrange
+        userRepository.save(user);
+
+        //act
+        User foundUser = userService.findOne(user.getId());
+
+        //assert
+        assertThat(foundUser.getUsername()).isEqualTo(user.getUsername());
+        assertThat(foundUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(foundUser.getPassword()).isEqualTo(user.getPassword());
+        assertThat(foundUser).isEqualTo(user);
+    }
+
+    @Test
+    public void findNonExistingUser() throws Exception {
+        Long nonExistingUserId = 2323L;
+
+        Optional<User> userFound = userRepository.findById(nonExistingUserId);
+
+        assertThat(userFound).isEmpty();
+        assertThatThrownBy(() -> userService.findOne(nonExistingUserId))
+                .isInstanceOf(UserNotFoundException.class);
+    }
+
+}
