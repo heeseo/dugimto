ad961e64c3cc4c432749a2b713ae28e890792896
Heekwon <seoheekwon@gmail.com>
2024-05-29 11:54:46 +0100

    feat: implement manual game creation using admin form

--- src/main/java/com/dugimto/controller/GameController.java
+++ src/main/java/com/dugimto/controller/GameController.java
@@ -1,19 +1,29 @@
 package com.dugimto.controller;
 
 import com.dugimto.domain.Game;
+import com.dugimto.domain.GameType;
 import com.dugimto.dto.GameForm;
+import com.dugimto.dto.OddsEntryDto;
+import com.dugimto.dto.OutcomeDto;
 import com.dugimto.service.GameService;
+import com.dugimto.service.OddsService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDateTime;
+
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class GameController {
 
     private final GameService gameService;
+    private final OddsService oddsService;
 
     @GetMapping("/games/new")
     public String createGameForm(Model model) {
@@ -22,9 +32,17 @@ public class GameController {
     }
 
     @PostMapping("/games/new")
-    public String createGame(GameForm gameForm) {
-//        Game game = gameForm.toGame();
+    public String createGame(GameForm gameForm, HttpServletRequest request) {
+        log.info("gameForm: {}", gameForm);
         gameService.createGame(gameForm);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/games/new/odds")
+    public String getOdds(Model model) {
+        String odds = oddsService.fetchUpcomingAndLiveOdds();
+        log.info("fetched odds: {}", odds);
         return "redirect:/";
     }
 }
--- src/main/java/com/dugimto/service/GameService.java
+++ src/main/java/com/dugimto/service/GameService.java
@@ -4,8 +4,10 @@ import com.dugimto.domain.Game;
 import com.dugimto.domain.GameType;
 import com.dugimto.dto.GameForm;
 import com.dugimto.exception.GameNotFoundException;
+import com.dugimto.mapper.GameMapper;
 import com.dugimto.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,14 +19,16 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class GameService {
 
     private final GameRepository gameRepository;
 
     @Transactional
     public Long createGame(GameForm gameForm) {
-        Game game = gameForm.toGame();
+        Game game = GameMapper.mapToGame(gameForm);
         gameRepository.save(game);
+        log.info("game: {}", game);
         return game.getId();
     }
 
